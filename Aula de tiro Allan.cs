1ae551523fc04eb69dbd98bf88cfc2ee6f39e01d
Leooka <leookajp99@hotmail.com>
2020-02-11 23:44:34 -0200

    first commit

---
+++ Aula de tiro Allan.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Aula1 : MonoBehaviour
+{
+    public float velocidade = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey( KeyCode.A))
+        {
+            transform.Translate(-1 * velocidade, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(1 * velocidade, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(0, -1 * velocidade, 0);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(0, 1 * velocidade, 0);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //this.gameObject.name --> personagem
+        //other.gameObject.name --> inimigo
+
+        if (other.gameObject.name == "Inimigo")
+            Destroy(other.gameObject);
+    }
+}
+
+
+
